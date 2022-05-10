812dffd5352f5d5bed132cfef8e469592e3c9819
rinkyFulwaniCSUN <rinky-gokuldas.fulwani.019@my.csun.edu>
2022-05-10 10:47:58 -0700

    UI Changes
    Added code to upload a file and predict in traffic signs module.

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from website import create_app;
 app = create_app();
 if __name__ == '__main__':
     app.run(debug=True);
+
+app.config['IMAGE_UPLOAD'] = 'C:/Users/13039/Desktop/RINKY/RINKY_GRAD/sem 2/COMP 680_MAchine_learning/COMP680_Autonomous_Driving/traffic_sign/test_images'
--- website/views.py
+++ website/views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template, request;
+from flask import Blueprint, render_template, request, jsonify;
+from flask_cors import CORS, cross_origin
 import sys
 import os
 sys.path.append(os.path.abspath("C:/Users/13039/Desktop/RINKY/RINKY_GRAD/sem 2/COMP 680_MAchine_learning/COMP680_Autonomous_Driving"))
-from traffic_sign.execute_traffic_sign import trafficsign
+from traffic_sign.execute_traffic_sign import trafficSign
+from website.decode import decodeImage
+#from werkzeug.utils import secure_filename
 
 #import trafficSign;
-
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 views = Blueprint('views', __name__)
 
@@ -15,7 +19,7 @@ def home():
 
 @views.route('/traffic_sign')
 def traffic_sign():
-    return render_template('traffic_sign.html', result=[{}]);
+    return render_template('index.html', result=[{}]);
 
 
 
@@ -23,10 +27,21 @@ def traffic_sign():
 def predict():
     print(request.method)
     print(request.form)
+    print(request.files)
+
     if request.method == "POST":
-        result = trafficsign()
+
+        # image = request.files['file']
+        # print(image)
+        # #filename = secure_filename(image.filename)
+        # basedirectory = os.path.abspath(os.path.dirname(__file__))
+        # image.save(os.path.join(basedirectory, app.config['IMAGE_UPLOAD'], image.filename))
+        image = request.json['image']
+        decodeImage(image, "inputImage.jpg")
+        result = trafficSign.trafficsign("inputImage.jpg")
         print("from views.py==", result)
-        return render_template('traffic_sign.html', result=result)
+        return jsonify(result)
+        #return render_template('traffic_sign.html', result=jsonify(result))
     else:
         return render_template('traffic_sign.html', result='')
 
