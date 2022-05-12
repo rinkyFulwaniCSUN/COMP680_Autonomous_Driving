b67feea127d05e476b467fb39ff7a6596582906d
rinkyFulwaniCSUN <rinky-gokuldas.fulwani.019@my.csun.edu>
2022-05-12 16:50:46 -0700

    Adding UI changes for signs and lights
    Adding UI changes for signs and lights

--- website/lights.py
+++ website/lights.py
@@ -2,13 +2,14 @@ from flask import Blueprint, render_template, request;
 import sys
 import os
 sys.path.append(os.path.abspath("C:/Users/13039/Desktop/RINKY/RINKY_GRAD/sem 2/COMP 680_MAchine_learning/COMP680_Autonomous_Driving/traffic_signal"))
-import detect_traffic_light
+from detect_traffic_light import TrafficLights
+from website.decode import decodeImage
 
 auth = Blueprint('auth', __name__);
 
 @auth.route('/traffic_light')
 def traffic_lights():
-    return render_template('traffic_lights.html', result=[{}]);
+    return render_template('trafficLights.html');
 
 
 @auth.route('/predictLights', methods=["POST"])
@@ -16,8 +17,7 @@ def predict():
     print(request.method)
     print(request.form)
     if request.method == "POST":
-        result = detect_traffic_light.traffic_lights()
-        print("from views.py==", result)
-        return render_template('traffic_lights.html', result=result)
-    else:
-        return render_template('traffic_lights.html', result='')
+        image = request.json['image']
+        decodeImage(image, "inputImage.jpg")
+        TrafficLights.traffic_lights("inputImage.jpg")
+        return "Success"
